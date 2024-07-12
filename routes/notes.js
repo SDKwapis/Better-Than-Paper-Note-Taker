1ba579b6c05b4cf3ac724d06c9a0677bbf4b1b97
SDKwapis <165666421+SDKwapis@users.noreply.github.com>
2024-07-11 22:02:36 -0400

    modularized code, added routes

---
+++ routes/notes.js
@@ -0,0 +1,53 @@
+const fb = require('express').Router();
+const fs = require('fs');
+const uuid = require('./uuid.js');
+const db = require('./db/db.json');
+
+
+fb.get('/api/notes', (req, res) => {
+    return res.json(db);
+  });
+  
+  fb.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request to add new note received!`);
+    const { title, text } = req.body;
+  
+    if (title && text) {
+      const newNote = {
+        title,
+        text,
+        note_id: uuid(),
+      };
+  
+      fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+      
+          const noteString = JSON.parse(data);
+  
+          noteString.push(newNote);
+  
+          fs.writeFile(
+            './db/db.json',
+            JSON.stringify(noteString, null, 4),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully updated db.json!')
+          );
+        }
+      });
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+      res.status(500).json('Error posting new note');
+    }
+  });
+
+  module.exports = fb;
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const db = require('./db/db.json');
+// const db = require('./db/db.json');
 const path = require('path');
-const uuid = require('./uuid');
+// const fs = require('fs');
+// const uuid = require('./uuid');
+const api = require('./routes/index.js');
 
 const PORT = 3001;
 const app = express();
 
-app.use(express.static('public'));
 
+app.use(express.static('public'));
+app.use('/api', api);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -20,29 +23,51 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
-app.get('/api/notes', (req, res) => {
-  return res.json(db);
-});
+// app.get('/api/notes', (req, res) => {
+//   return res.json(db);
+// });
 
-app.post('/api/notes', (req, res) => {
-  console.info(`${req.method} request to add new note received!`);
-  const { title, text} = req.body;
+// app.post('/api/notes', (req, res) => {
+//   console.info(`${req.method} request to add new note received!`);
+//   const { title, text } = req.body;
 
-  if (title && text) {
-    const newNote = {
-      title,
-      text,
-      note_id: uuid(),
-    };
-    const response = {
-      status: 'success',
-      body: newNote,
-    };
-    res.status(201).json(response);
-  } else {
-    res.status(500).json('Error posting new note');
-  }
-});
+//   if (title && text) {
+//     const newNote = {
+//       title,
+//       text,
+//       note_id: uuid(),
+//     };
+
+//     fs.readFile('./db/db.json', 'utf8', (err, data) => {
+//       if (err) {
+//         console.error(err);
+//       } else {
+    
+//         const noteString = JSON.parse(data);
+
+//         noteString.push(newNote);
+
+//         fs.writeFile(
+//           './db/db.json',
+//           JSON.stringify(noteString, null, 4),
+//           (writeErr) =>
+//             writeErr
+//               ? console.error(writeErr)
+//               : console.info('Successfully updated db.json!')
+//         );
+//       }
+//     });
+
+//     const response = {
+//       status: 'success',
+//       body: newNote,
+//     };
+//     console.log(response);
+//     res.status(201).json(response);
+//   } else {
+//     res.status(500).json('Error posting new note');
+//   }
+// });
 
 app.listen(PORT, () => 
     console.log(`LISTENING @ http://localhost:${PORT}`)
