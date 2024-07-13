7537baa59fe3a332da559cf6b20dbb126b18712d
Stephan Kwpais <stephankwpais@Stephans-Mac-mini.local>
2024-07-13 10:53:43 -0400

    tried getting routes to work again, was unsuccessful, updating repo just incase

--- server.js
+++ server.js
@@ -1,3 +1,37 @@
+// const express = require('express');
+// const path = require('path');
+// const api = require('./routes/index.js');
+// const notes = require ('./routes/notes.js');
+
+
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static('public'));
+// app.use('/api', api);
+// app.use('/api/notes', notes);
+
+
+// app.get('/', (req, res) => {
+//  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+
+// app.get('/notes', (req, res) => {
+//  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+// });
+
+
+// app.listen(PORT, () => {
+//  console.log(`LISTENING @ http://localhost:${PORT}`);
+// });
+
+
+
+
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
